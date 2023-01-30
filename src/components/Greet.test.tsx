4e6723151d5d3d071349839d6c1c851390f17f38
Shubham Patrick <ahthneeuhl@gmail.com>
2023-01-31 00:00:11 +0530

    Grouping Tests

--- src/components/Greet.test.tsx
+++ src/components/Greet.test.tsx
@@ -1,14 +1,18 @@
 import { render, screen } from "@testing-library/react";
 import Greet from "./Greet";
 
-test("Greet renders correctly", () => {
-  render(<Greet />);
-  const textElement = screen.getByText(/hello/i);
-  expect(textElement).toBeInTheDocument();
+describe("Greet", () => {
+  test("renders correctly", () => {
+    render(<Greet />);
+    const textElement = screen.getByText(/hello/i);
+    expect(textElement).toBeInTheDocument();
+  });
 });
 
-test("Greet renders with name", () => {
-  render(<Greet name="Arya" />);
-  const textElement = screen.getByText("Hello Arya");
-  expect(textElement).toBeInTheDocument();
+describe("Nested", () => {
+  test("renders with name", () => {
+    render(<Greet name="Arya" />);
+    const textElement = screen.getByText("Hello Arya");
+    expect(textElement).toBeInTheDocument();
+  });
 });
