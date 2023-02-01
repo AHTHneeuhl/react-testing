7afb38c696bab62ceaab82802edc4e963519f744
Shubham Patrick <ahthneeuhl@gmail.com>
2023-02-01 22:42:10 +0530

    Custom Render Function

--- src/components/mui/MuiMode.test.tsx
+++ src/components/mui/MuiMode.test.tsx
@@ -1,12 +1,9 @@
 import { render, screen } from "@testing-library/react";
-import { AppProviders } from "../../providers/AppProviders";
 import MuiMode from "./MuiMode";
 
 describe("MuiMode", () => {
   test("renders text correctly", () => {
-    render(<MuiMode />, {
-      wrapper: AppProviders,
-    });
+    render(<MuiMode />);
     const headingElement = screen.getByRole("heading");
     expect(headingElement).toHaveTextContent("light mode");
   });
