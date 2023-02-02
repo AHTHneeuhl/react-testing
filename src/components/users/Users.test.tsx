f5fcf7229205bf445e0b593b505370f54341c637
Shubham Patrick <ahthneeuhl@gmail.com>
2023-02-02 23:31:41 +0530

    MSW Error Handling

--- src/components/users/Users.test.tsx
+++ src/components/users/Users.test.tsx
@@ -1,4 +1,6 @@
 import { render, screen } from "@testing-library/react";
+import { rest } from "msw";
+import { server } from "../../mocks/server";
 import { Users } from "./Users";
 
 describe("Users", () => {
@@ -12,5 +14,19 @@ describe("Users", () => {
     render(<Users />);
     const users = await screen.findAllByRole("listitem");
     expect(users).toHaveLength(3);
+  });
+
+  test("renders error", async () => {
+    server.use(
+      rest.get(
+        "https://jsonplaceholder.typicode.com/users",
+        (req, res, ctx) => {
+          return res(ctx.status(500));
+        }
+      )
+    );
+    render(<Users />);
+    const errorText = await screen.findByText("Error fetching users");
+    expect(errorText).toBeInTheDocument();
   });
 });
